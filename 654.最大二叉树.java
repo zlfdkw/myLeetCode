b8117359c1b7fe07c7785d30088c879c3fda392c
zlfdkw <2082818450@qq.com>
2023-05-02 23:56:45 +0800

    最大二叉树

---
+++ 654.最大二叉树.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=java
+ *
+ * [654] 最大二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    TreeNode build(int[] nums, int lo, int hi) {
+        if(lo > hi) {
+            return null;
+        }
+        int index = -1;int maxVal = Integer.MIN_VALUE;
+        for(int i = lo;i <= hi;i++) {
+            if(maxVal < nums[i]) {
+                maxVal = nums[i];
+                index = i;
+            }
+        }
+        TreeNode root = new TreeNode(maxVal);
+        root.left = build(nums, lo, index - 1);
+        root.right = build(nums, index + 1, hi);
+        return root;
+    }
+}
+// @lc code=end
+
