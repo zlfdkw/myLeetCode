9ba56568150a6e58a2818e521aedbec2f41ba5ce
zlfdkw <2082818450@qq.com>
2023-08-26 00:14:29 +0800

    2023/8/26

--- 322.零钱兑换.java
+++ 322.零钱兑换.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode.cn id=322 lang=java
  *
@@ -6,20 +8,25 @@
 
 // @lc code=start
 class Solution {
+    int[] memo;
     public int coinChange(int[] coins, int amount) {
+        memo = new int[amount + 1];
+        Arrays.fill(memo, -666);
         return dp(coins, amount);
     }
 
     public int dp(int[] coins, int amount) {
         if(amount == 0) return 0;
         if(amount < 0) return -1;
+        if(memo[amount] != -666) return memo[amount];
         int res = Integer.MAX_VALUE;
         for (int coin : coins) {
             int temp = dp(coins, amount - coin);
             if(temp == -1) continue;
             res = Math.min(res, temp + 1);
         }
-        return res == Integer.MAX_VALUE ? -1 : res;
+        memo[amount] = res == Integer.MAX_VALUE ? -1 : res;
+        return memo[amount];
     } 
 }
 // @lc code=end
