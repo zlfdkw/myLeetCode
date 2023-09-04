364c80d0e972d911ee05f5f0153e49962292133f
zlfdkw <2082818450@qq.com>
2023-09-05 00:16:52 +0800

    2023/9/5

--- 111.二叉树的最小深度.java
+++ 111.二叉树的最小深度.java
@@ -1,3 +1,10 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+import javafx.scene.Node;
+
 /*
  * @lc app=leetcode.cn id=111 lang=java
  *
@@ -22,7 +29,24 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
 
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0;i < size;i++) {
+                TreeNode temp = queue.poll();
+                if(temp.left == null && temp.right == null) {
+                    return depth;
+                }
+                if(temp.left != null) queue.offer(temp.left);
+                if(temp.right != null) queue.offer(temp.right);
+            }
+            depth++;
+        }
+        return depth;
     }
 }
 // @lc code=end
